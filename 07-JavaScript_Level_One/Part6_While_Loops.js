ee5fb6b63929b9eabf795982c4ba3aa721e11ef9
Jordan Stern <jordan.e.stern@gmail.com>
2022-12-22 14:04:45 -0700

    completed simple while loop exercise

--- 07-JavaScript_Level_One/Part6_While_Loops.js
+++ 07-JavaScript_Level_One/Part6_While_Loops.js
@@ -48,3 +48,13 @@ while(x < 5){
 // QUICK EXERCISE
 
 // Write a while loop that prints out only the even numbers from 1 to 10.
+var x = 1
+
+while (x <= 10) {
+
+    if (x % 2 === 0) {
+        console.log(x);
+    }
+
+    x = x + 1;
+}
