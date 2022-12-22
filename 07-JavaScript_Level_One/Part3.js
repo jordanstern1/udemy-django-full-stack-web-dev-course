73f52a756f6955be8cc9025ccebd1ad11f446627
Jordan Stern <jordan.e.stern@gmail.com>
2022-12-22 13:04:53 -0700

    filled in simple sample js script connected to an HTML file

---
+++ 07-JavaScript_Level_One/Part3.js
@@ -0,0 +1,5 @@
+// note that single and double quotes are interchangeable as in Python
+var weightInPounds = prompt('What is your weight in pounds?')
+var weightInKilos = weightInPounds * 0.454
+alert("Your weight in kilograms is " + weightInKilos)
+console.log("Conversion Completed")
