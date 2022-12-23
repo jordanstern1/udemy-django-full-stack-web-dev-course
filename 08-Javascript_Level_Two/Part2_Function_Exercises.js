9d2cfb08209baf326707455ca1f5f5d97b29902b
Jordan Stern <jordan.e.stern@gmail.com>
2022-12-22 18:24:42 -0700

    finished exercise 4

--- 08-Javascript_Level_Two/Part2_Function_Exercises.js
+++ 08-Javascript_Level_Two/Part2_Function_Exercises.js
@@ -85,9 +85,19 @@ function stringTimes(str, n) {
 // luckySum(1, 2, 13) → 3
 // luckySum(1, 13, 3) → 1
 
-function luckySum(a, b, c){
-
-  //Code Goes Here
+function luckySum(a, b, c) {
+  if (a === 13) {
+    return 0;
+  }
+  else if (b === 13) {
+    return a;
+  }
+  else if (c === 13) {
+    return a + b;
+  }
+  else {
+    return a + b + c;
+  }
 }
 
 // PROBLEM 5:
