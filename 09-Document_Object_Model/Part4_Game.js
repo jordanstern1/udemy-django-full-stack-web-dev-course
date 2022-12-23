4fe9b86e4e8cfe8687361dc15ae07a1944727d22
Jordan Stern <jordan.e.stern@gmail.com>
2022-12-23 14:14:09 -0700

    cleaned up indentation for readability

--- 09-Document_Object_Model/Part4_Game.js
+++ 09-Document_Object_Model/Part4_Game.js
@@ -3,14 +3,19 @@ var restart = document.querySelector(".btn")
 
 td.forEach(
   function(item) {
-    item.addEventListener('click', function () {
-    if (item.textContent === "") {
-      item.textContent = 'X';
-    } else if (item.textContent === "X") {
-      item.textContent = 'O';
-    } else if (item.textContent === "O") {
-      item.textContent = '';
-    }
-  })})
+    item.addEventListener(
+      'click', 
+      function () {
+        if (item.textContent === "") {
+          item.textContent = 'X';
+        } else if (item.textContent === "X") {
+          item.textContent = 'O';
+        } else if (item.textContent === "O") {
+          item.textContent = '';
+        }
+      }
+    )
+  }
+)
 
 restart.addEventListener('click', function () {td.forEach(function(x) {x.textContent = ""})})
