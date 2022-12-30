953d0d009b22ce75736428a2d2dad000c33d882d
Jordan Stern <jordan.e.stern@gmail.com>
2022-12-30 16:48:21 -0600

    more progress

--- 10-jQuery/Part3_FrontEndProject.js
+++ 10-jQuery/Part3_FrontEndProject.js
@@ -7,20 +7,24 @@ var player1Color = 'rgb(237, 45, 73)'
 var game_on = true;
 var table = $('table tr');
 
+// http://stackoverflow.com/questions/6139407/getting-td-by-index-with-jquery
 function reportWin(rowNum, colNum) {
   console.log("You won starting at this row, col");
   console.log(rowNum);
   console.log(colNum);
 }
 
+// Change the color of a particular button in the table
 function changeColor(rowIndex, colIndex, color) {
   return table.eq(rowIndex).find('td').eq(colIndex).find('button').css('background-color', color);
 }
 
+// Report back current color of a particular button
 function returnColor(rowIndex, colIndex, color) {
   return table.eq(rowIndex).find('td').eq(colIndex).find('button').css('background-color');
 }
 
+// given a column index, returns the bottom row that is still gray
 function checkBottom(colIndex) {
   var colorReport = returnColor(5, colIndex);
   for (var row = 5; i >= 0; row--) {
@@ -31,6 +35,59 @@ function checkBottom(colIndex) {
   }
 }
 
+// Check to see if 4 inputs are the same color (will help us check horizontal, diagonal, or vertical win)
 function colorMatchCheck(one, two, three, four) {
   return (one === two && one === three && one === four && one !== 'rgb(128, 128, 128)' & one !== 'undefined')
+}
+
+
+// Check for Horizontal Wins
+function horizontalWinCheck() {
+  // for each row, iterate across the columns to check for four of the same (red or blue) colors in a row
+  // note we only have to check the first four buttons in each row to look for a horizontal win
+  for (var row = 0; row < 6; row++) {
+    for (var col = 0; col < 4; col++) {
+      if (colorMatchCheck(returnColor(row,col), returnColor(row,col+1) ,returnColor(row,col+2), returnColor(row,col+3))) {
+        console.log('horiz');
+        reportWin(row,col);
+        return true;
+      }else {
+        continue;
+      }
+    }
+  }
+}
+
+// Check for Vertical Wins
+function verticalWinCheck() {
+  for (var col = 0; col < 7; col++) {
+    for (var row = 0; row < 3; row++) {
+      if (colorMatchCheck(returnColor(row,col), returnColor(row+1,col) ,returnColor(row+2,col), returnColor(row+3,col))) {
+        console.log('vertical');
+        reportWin(row,col);
+        return true;
+      }else {
+        continue;
+      }
+    }
+  }
+}
+
+// Check for Diagonal Wins
+function diagonalWinCheck() {
+  for (var col = 0; col < 5; col++) {
+    for (var row = 0; row < 7; row++) {
+      if (colorMatchCheck(returnColor(row,col), returnColor(row+1,col+1) ,returnColor(row+2,col+2), returnColor(row+3,col+3))) {
+        console.log('diag');
+        reportWin(row,col);
+        return true;
+      }else if (colorMatchCheck(returnColor(row,col), returnColor(row-1,col+1) ,returnColor(row-2,col+2), returnColor(row-3,col+3))) {
+        console.log('diag');
+        reportWin(row,col);
+        return true;
+      }else {
+        continue;
+      }
+    }
+  }
 }
