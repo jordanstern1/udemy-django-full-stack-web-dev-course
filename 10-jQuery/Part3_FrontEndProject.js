b03efc971d6979cfcf1d0c9d1ce319f880d64c7e
Jordan Stern <jordan.e.stern@gmail.com>
2022-12-30 14:07:32 -0600

    connect 4 project started

---
+++ 10-jQuery/Part3_FrontEndProject.js
@@ -0,0 +1,52 @@
+const { _round } = require("regression");
+
+var p1 = prompt("Player One: Enter Your Name, you will be Blue")
+var p2 = prompt("Player Two: Enter Your Name, you will be Red")
+
+var i = 1 // index to count how many moves have been played
+var className = 'turnBlue';
+var lastClicks = 
+
+$('.circle').click(function() {
+  var id = $(this).attr('id');
+  remainder = id % 7;
+  colNum = (remainder === 0) ? 7 : remainder;
+
+  
+  if (i % 2 == 0) {
+    className = 'turnBlue';
+  }
+  else {
+    className = 'turnRed';
+  }
+
+  console.log('i' + i)
+  console.log('id' + id)
+  
+  $('#' + id).toggleClass(className)
+ 
+  i++;
+  
+});
+
+
+// $('h3').text()
+
+var circle_id = $('.circle').click(function() {
+  i++;
+  if (i % 2 == 0) {
+    className = 'turnBlue';
+  }
+  else {
+    className = 'turnRed';
+  }
+  $(this).data("id");
+})
+
+var click_count = 0;
+remainder = click_count % 7;
+if (remainder == 1) {
+
+} else if (remainder == 2) {
+  
+}
