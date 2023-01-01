37692fa133a079f45fb131e9c25b685d5ffc0f8a
Jordan Stern <jordan.e.stern@gmail.com>
2023-01-01 15:15:01 -0700

    finished game logic

--- 10-jQuery/Part3_FrontEndProject.js
+++ 10-jQuery/Part3_FrontEndProject.js
@@ -2,7 +2,7 @@ var player1 = prompt('Player One: Enter Your Name, you will be Blue');
 var player1Color = 'rgb(86, 151, 255)'
 
 var player2 = prompt('Player Two: Enter Your Name, you will be Red');
-var player1Color = 'rgb(237, 45, 73)'
+var player2Color = 'rgb(237, 45, 73)'
 
 var game_on = true;
 var table = $('table tr');
@@ -27,10 +27,10 @@ function returnColor(rowIndex, colIndex, color) {
 // given a column index, returns the bottom row that is still gray
 function checkBottom(colIndex) {
   var colorReport = returnColor(5, colIndex);
-  for (var row = 5; i >= 0; row--) {
+  for (var row = 5; row >= 0; row--) {
     colorReport = returnColor(row, colIndex)
       if (colorReport === 'rgb(128, 128, 128)') {
-        return row 
+        return row;
       }
   }
 }
@@ -79,13 +79,13 @@ function verticalWinCheck() {
 function diagonalWinCheck() {
   for (var col = 0; col < 5; col++) {
     for (var row = 0; row < 7; row++) {
-      // look for diagonal wins with positive slope
+      // checking for diagonal wins with negative slope
       if (colorMatchCheck(returnColor(row,col), returnColor(row+1,col+1) ,returnColor(row+2,col+2), returnColor(row+3,col+3))) {
         console.log('diag');
         reportWin(row,col);
         return true;
       }
-      // look for diagonal wins with negative slope
+      // checking for diagonal wins with positive slope 
       else if (colorMatchCheck(returnColor(row,col), returnColor(row-1,col+1) ,returnColor(row-2,col+2), returnColor(row-3,col+3))) {
         console.log('diag');
         reportWin(row,col);
@@ -96,3 +96,35 @@ function diagonalWinCheck() {
     }
   }
 }
+
+
+var currentPlayer = 1;
+var currentName = player1;
+var currentColor = player1Color;
+
+$('h3').text(player1 + ", it is your turn, pick a column to drop in!")
+
+$('.board button').on('click', function() {
+  var col = $(this).closest('td').index();
+  var bottomAvail = checkBottom(col);
+  changeColor(bottomAvail, col, currentColor);
+
+  // check for win or tie
+  if (horizontalWinCheck() || verticalWinCheck() || diagonalWinCheck()) {
+    $('h1').text(currentName + ", you have won! Please refresh the page to play again.")
+    $('h3').fadeOut('fast');
+    $('h2').fadeOut('fast');
+  }
+
+  currentPlayer = currentPlayer * -1;
+
+  if (currentPlayer === 1) {
+    currentName = player1;
+    $('h3').text(currentName + ", it is your turn")
+    currentColor = player1Color;
+  } else {
+    currentName = player2;
+    $('h3').text(currentName + ", it is your turn")
+    currentColor = player2Color;
+  }
+})
