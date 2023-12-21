d1b89e491fc6382f14f3a453037a96d4973aea20
Jordan Stern <jordan.e.stern@gmail.com>
2023-12-21 13:35:28 -0700

    added example of injecting data from model into a template

--- 17-Django_Level_Two/first_project/first_app/views.py
+++ 17-Django_Level_Two/first_project/first_app/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from first_app.models import Topic, Webpage, AccessRecord # import models so we can inject data from models into the view
 
 # Create your views here.
 
 def index(request):
-    # return HttpResponse("Hello World")
-    my_dict = {'insert_me': 'Hello, I am from first_app/index.html!'}
-    return render(request, 'first_app/index.html', context=my_dict)
+
+    webpages_list = AccessRecord.objects.order_by('date')
+    date_dict = {'access_records': webpages_list}
+
+    return render(request, 'first_app/index.html', context=date_dict)
