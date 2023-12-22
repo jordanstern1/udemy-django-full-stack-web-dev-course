feefc814304a3f98c733ad4a477a3fc39ce7c140
Jordan Stern <jordan.e.stern@gmail.com>
2023-12-22 15:05:41 -0700

    adding on_delete

--- 20-Django_Level_Five/learning_users/basic_app/models.py
+++ 20-Django_Level_Five/learning_users/basic_app/models.py
@@ -6,7 +6,9 @@ from django.contrib.auth.models import User # built-in Django User model
 class UserProfileInfo(models.Model):
     # default user doesn't have this attribute, so here we can extend the User class 
     # (don't use inheritance; can cause problems)
-    user = models.OneToOneField(User) 
+    user = models.OneToOneField(User, 
+                                on_delete=models.CASCADE # now required in later version of Django
+                                ) 
 
     # additional attributes added to User
     portfolio_site = models.URLField(blank=True)
@@ -14,7 +16,7 @@ class UserProfileInfo(models.Model):
     # blank = True so user isn't requires
     profile_pic = models.ImageField(upload_to='profile_pics', # folder where pics will be uploaded
                                     blank=True # so user isn't required to upload a pic
-    ) 
+                                    ) 
 
     def ___str__(self):
         return self.user.username
