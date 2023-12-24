6959cde1871f4e87413e1117786bce21d99599a1
Jordan Stern <jordan.e.stern@gmail.com>
2023-12-24 10:06:35 -0700

    set up: forms.py, settings.py, and static folder

---
+++ 23-Block_Clone_Project/mysite/blog/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+from blog.models import Post, Comment
+
+class PostForm(forms.ModelForm):
+    class Meta():
+        model = Post
+        fields = ('author', 'title', 'text')
+
+        # add in widgets and add in certain classes for CSS styling
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'textinputclass'}),
+
+            # NOTE: editable and medium-editor-text-area will be classes we'll get from medium (not classes we define)
+            'text': forms.Textarea(attrs={'class': 'editable medium-editor-textarea postcontent'})
+        }
+
+
+class CommentForm(forms.ModelForm):
+    
+    class Meta():
+        model = Comment 
+        fields = ('author', 'text')
+
+    # add in widgets and add in certain classes for CSS styling
+    widgets = {
+        'author': forms.TextInput(attrs={'class': 'textinputclass'}),
+        'text': forms.Textarea(attrs={'class': 'editable medium-editor-textarea'})
+    }
+
+
