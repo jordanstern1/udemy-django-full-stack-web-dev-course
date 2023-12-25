5dc8945c38e17627f93427f718f53c32675992c5
Jordan Stern <jordan.e.stern@gmail.com>
2023-12-25 01:21:06 -0700

    fixed directory structure and started setting up login/signup in accounts app

--- 24-Social_Clone_Project/simple_social_clone/simplesocial/accounts/views.py
+++ 24-Social_Clone_Project/simple_social_clone/simplesocial/accounts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from . import forms
+from django.views.generic import CreateView
 
 # Create your views here.
+class SignUp(CreateView):
+    form_class = forms.UserCreateForm # not instantiating class
+    success_url = reverse_lazy('login') # lazily evaluate so doesn't execute until user has signed up
+    template_name = 'accounts/signup.html'
