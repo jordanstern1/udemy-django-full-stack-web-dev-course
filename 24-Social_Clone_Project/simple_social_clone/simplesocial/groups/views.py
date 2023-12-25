3b9bc301e7fa40a09f04aed455b9e234c5007641
Jordan Stern <jordan.e.stern@gmail.com>
2023-12-25 11:23:04 -0700

    fleshed out more templates and views for groups and posts apps

--- 24-Social_Clone_Project/simple_social_clone/simplesocial/groups/views.py
+++ 24-Social_Clone_Project/simple_social_clone/simplesocial/groups/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import (LoginRequiredMixin, 
+                                        PermissionRequiredMixin)
+from django.urls import reverse
+from django.views import generic
+from groups.models import Group, GroupMembers
 
 # Create your views here.
+
+class CreateGroup(LoginRequiredMixin, generic.CreateView):
+    fields = ('name', 'description') # only editable fields
+    model = Group # connect to group model
+
+class SingleGroup(generic.DetailView):
+    model = Group
+
+class ListGroups(generic.ListView):
+    model = Group
