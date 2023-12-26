4975396d9573040d129ae762aad3a5e55e09f179
Jordan Stern <jordan.e.stern@gmail.com>
2023-12-25 22:43:48 -0700

    example showing how to reorder fields in admin

--- 26-Customizing_The_Django_Admin/test/my_video_rental/videos/admin.py
+++ 26-Customizing_The_Django_Admin/test/my_video_rental/videos/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
+
+class MovieAdmin(admin.ModelAdmin):
+    # this will reorder fields in detail view for a Movie record in admin
+    # rather than using default ordering which will match ordering of fields in our model definition
+    fields = ['release_year', 'title', 'length'] 
+
 admin.site.register(models.Customer)
-admin.site.register(models.Movie)
+admin.site.register(models.Movie, MovieAdmin)
